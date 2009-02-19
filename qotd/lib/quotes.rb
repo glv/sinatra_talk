449458209dc022167f73a3fd66aff4734ee20a9c
Glenn Vanderburg <glv@vanderburg.org>
2009-02-19 09:09:44 -0500

    Initial checkin of qotd.

---
+++ qotd/lib/quotes.rb
@@ -0,0 +1,32 @@
+require 'yaml'
+
+class Quotes
+  def self.load(fn)
+    self.new(fn)
+  end
+  
+  def initialize(fn=nil)
+    if fn
+      load(fn)
+    end
+  end
+  
+  def load(fn)
+    puts "Loading quotes file '#{fn}'"
+    @qlist = YAML.load(IO.read(fn))
+    @qlist.each_with_index{|qhash, i| qhash[:id] = i}
+  end
+  
+  def [](id)
+    @qlist[id]
+  end
+  
+  def random(seed=nil)
+    srand(seed) if seed
+    @qlist[rand(@qlist.size)]
+  end
+  
+  def search(re, key)
+    @qlist.select {|qhash| qhash[key] =~ re }
+  end
+end
